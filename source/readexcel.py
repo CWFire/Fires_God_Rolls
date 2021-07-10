e77f6254beaa58b169c19c145a98d0bb5e78ab64
Joseph Clack <joe@joeclack.me.uk>
2021-07-10 04:51:24 +0100

    add generated_God_Rolls.txt introduce basic generate functionality to main.py

--- source/readexcel.py
+++ source/readexcel.py
@@ -5,5 +5,7 @@ workbook_path = path.join(getcwd(), "..", "God Rolls.xlsx",)
 
 workbook = load_workbook(filename=workbook_path, data_only=True)
 for sheet in workbook:
-    print(sheet["A1"].value)
+    for row in sheet:
+        gun = row[0].value
+
 
