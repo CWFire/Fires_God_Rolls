18cec7f9c8a60c23cc9dabc1b5625fe69801fd80
Joseph Clack <joe@joeclack.me.uk>
2021-07-12 00:43:13 +0100

    .gitignore - set to ignore pydest destiny manifest getperkhash.py - update example main.py - increase functionality + redundancy

--- source/getperkhash.py
+++ source/getperkhash.py
@@ -7,9 +7,16 @@ load_dotenv()
 
 loop = asyncio.get_event_loop()
 
-api_key = getenv("API_KEY", loop)
+api_key = getenv("API_KEY")
 
-destiny = pydest.Pydest(api_key)
-resp = loop.run_until_complete(destiny.api.search_destiny_entities("DestinyInventoryItemDefinition", "vision of confluence (timelost)"))
-perkhash = resp["Response"]["results"]["results"][0]["hash"]
+destiny = pydest.Pydest(api_key, loop)
+resp = loop.run_until_complete(destiny.api.search_destiny_entities("DestinyInventoryItemDefinition", "Crown Splitter"))
+results = resp["Response"]["results"]["results"]
+hashes = [result["hash"] for result in results]
+dehashed = [loop.run_until_complete(destiny.decode_hash(itemhash, "DestinyInventoryItemDefinition")) for itemhash in hashes]
+
+
+# we can differentiate between weapons / perks / shaders using the decoded items' property 'traidIds'
+# weapons have item_type.weapon in traitIds
+# perks do not have property traitIds
 
--- source/main.py
+++ source/main.py
@@ -10,8 +10,8 @@ description:This is a list I have created to highlight all the "God Rolls" of we
 load_dotenv()
 
 loop = asyncio.get_event_loop()
-api_key = getenv("API_KEY", loop)
-destiny = pydest.Pydest(api_key)
+api_key = getenv("API_KEY")
+destiny = pydest.Pydest(api_key, loop)
 
 workbook_path = path.join(getcwd(), "..", "God Rolls.xlsx")
 workbook = load_workbook(filename=workbook_path, data_only=True)
@@ -27,11 +27,28 @@ current_gun_rolls_string = ""
 
 perk_hashes_cache = {}
 
+valid_item_types = ["Weapon", "Perk"]
 
-async def get_inventory_item_hash(query: str):
+
+async def get_inventory_item(query: str, item_type: str = None):
+    global valid_item_types
+    item_type = item_type if item_type in valid_item_types else valid_item_types[0]
     resp = await destiny.api.search_destiny_entities("DestinyInventoryItemDefinition", query[:30])
     results = resp["Response"]["results"]["results"]
-    return results[0]["hash"] if results else None
+    if not results:
+        return None
+    else:
+        dehashed_results = [await destiny.decode_hash(result["hash"], "DestinyInventoryItemDefinition") for result in results]
+        valid_results = dehashed_results
+        if item_type == "Weapon":
+            valid_results = [data for data in dehashed_results if "traitIds" in data.keys() and "item_type.weapon" in data["traitIds"]]
+        elif item_type == "Perk":
+            valid_results = [data for data in dehashed_results if "traitIds" not in data.keys()]
+        valid_results = [data for data in valid_results if "iconWatermarkShelved" not in data.keys()]
+        if len(valid_results) > 1:
+            print(f"Amibiguous results from query '{query}'")
+            [print(f"\t{valid_result}") for valid_result in valid_results]
+        return valid_results[0]
 
 
 async def get_perk_hash(query: str):
@@ -39,23 +56,25 @@ async def get_perk_hash(query: str):
     if query in perk_hashes_cache.keys():
         return perk_hashes_cache[query]
     else:
-        perk_hash = await get_inventory_item_hash(query)
+        perk_item = await get_inventory_item(query, "Perk")
+        perk_hash = perk_item["hash"]
         perk_hashes_cache[query] = perk_hash
         return perk_hash
 
+
 for sheet in workbook:
     with open(output_path, "a") as appendfile:
-        appendfile.write(f"\n\n//{sheet.title}")
+        appendfile.write(f"\n\n\n//{sheet.title}")
 
     for row in tuple(sheet)[1:]:
         gun = row[0].value
         if gun and str(gun).strip():
-            current_gun_name = gun
+            gun_item = loop.run_until_complete(get_inventory_item(gun))
+            current_gun_name = gun_item["displayProperties"]["name"]
+            current_gun_hash = gun_item["hash"]
             with open(output_path, "a") as appendfile:
                 appendfile.write(current_gun_rolls_string)
                 appendfile.write(f"\n\n//{current_gun_name}\n//notes:God Roll")
-            current_gun_hash = loop.run_until_complete(get_inventory_item_hash(gun))
-            current_gun_rolls_string = ""
 
         perk_names = [cell.value for cell in row[1:5] if cell.value]
         perk_hashes_str = str([loop.run_until_complete(get_perk_hash(perk_name)) for perk_name in perk_names if perk_name and perk_name != "*"])[1:-1].replace(" ", "")
@@ -74,3 +93,4 @@ for sheet in workbook:
 
     with open(output_path, "a") as appendfile:
         appendfile.write(current_gun_rolls_string)
+    current_gun_rolls_string = ""
